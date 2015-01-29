e81a0cc150667a64048878e56c3d9c5dfb85ae7a
Yegor Kolesnikov <kelegorkel@gmail.com>
2015-01-29 12:03:04 +0200

    added search controller

--- js/orfer_food.js
+++ js/orfer_food.js
@@ -3,5 +3,26 @@
 (function () {
   "use strict";
   
+  var results = [
+    {
+      item: "pizza",
+      addr: "Kharkov"
+    }
+  ];
+  
+  //Angular application
   var app = angular.module("order_food", []);
+  
+  // Search controller
+  app.controller("search_ctrl", function () {
+    this.result = {};
+    this.results = results;
+    this.visible = true;
+    this.submit = function () {
+      if (this.result.item !== undefined && this.result.addr !== undefined) {
+        this.visible = false;
+      }
+    };
+  });
+  
 }());
